23fcd8249bb500d4b2cd0c4f2313ff6720bd0f85
Daniel Ogunlere <dogunlere@gmail.com>
2023-09-15 17:09:00 +0100

    Minor change to the HMTL and Javascript file

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,14 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.google.com", "wwww.yahoo.com"]
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEL = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
-    console.log("Clicked from Addeventlistner")
+    myLeads.push(inputEl.value)
+    console.log(myLeads) 
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEL.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
